6ee2282c25267b13539a9d8466e613a61c0f2f85
ttyobiwan <piotr.tobiasz.dev@gmail.com>
2025-09-04 18:41:31 +0200

    add cart tests

--- test/mdstore_web/live/products_live/show_test.exs
+++ test/mdstore_web/live/products_live/show_test.exs
@@ -13,6 +13,7 @@ defmodule MdstoreWeb.ProductsLive.ShowTest do
       assert html =~ product.name
       assert html =~ Images.get_image_link(product.front_image)
       assert html =~ "Buy now"
+      assert html =~ "Add to cart"
     end
 
     test "shows out of stock product", %{conn: conn} do
@@ -22,6 +23,36 @@ defmodule MdstoreWeb.ProductsLive.ShowTest do
       assert html =~ "Out of Stock"
       assert html =~ "Notify"
       refute html =~ "Buy now"
+      refute html =~ "Add to cart"
+    end
+  end
+
+  describe "add to cart flow" do
+    setup %{conn: conn} do
+      user = user_fixture()
+      conn = log_in_user(conn, user)
+      product = product_fixture()
+      %{conn: conn, user: user, product: product}
+    end
+
+    test "adds product to cart successfully", %{conn: conn, product: product} do
+      {:ok, lv, _html} = live(conn, ~p"/products/#{product.id}")
+
+      lv |> element("button", "Add to cart") |> render_click()
+
+      assert render(lv) =~ "Product added to cart successfully"
+    end
+
+    test "shows message when product is already in cart", %{conn: conn, product: product} do
+      {:ok, lv, _html} = live(conn, ~p"/products/#{product.id}")
+
+      # Add to cart first time
+      lv |> element("button", "Add to cart") |> render_click()
+      assert render(lv) =~ "Product added to cart successfully"
+
+      # Try to add the same product again
+      lv |> element("button", "Add to cart") |> render_click()
+      assert render(lv) =~ "Product is already in the cart"
     end
   end
 
@@ -96,9 +127,27 @@ defmodule MdstoreWeb.ProductsLive.ShowTest do
 
       assert render(lv) =~ "Something went wrong"
     end
+
+    test "hides add to cart and buy now buttons when payment intent is present", %{
+      conn: conn,
+      product: product
+    } do
+      {:ok, lv, _html} = live(conn, ~p"/products/#{product.id}")
+
+      # Initially both buttons should be present
+      assert has_element?(lv, "button", "Buy now")
+      assert has_element?(lv, "button", "Add to cart")
+
+      # Start purchase flow
+      lv |> element("button", "Buy now") |> render_click()
+
+      # After starting purchase, buttons should be hidden
+      refute has_element?(lv, "button", "Buy now")
+      refute has_element?(lv, "button", "Add to cart")
+    end
   end
 
-  describe "unauthenticated payment flow" do
+  describe "unauthenticated flows" do
     test "redirects to login when trying to purchase", %{conn: conn} do
       product = product_fixture()
       {:ok, lv, _html} = live(conn, ~p"/products/#{product.id}")
@@ -106,6 +155,23 @@ defmodule MdstoreWeb.ProductsLive.ShowTest do
       lv |> element("button", "Buy now") |> render_click()
 
       assert_redirected(lv, ~p"/users/log-in?next=/products/#{product.id}")
+    end
+
+    test "redirects to login when trying to add to cart", %{conn: conn} do
+      product = product_fixture()
+      {:ok, lv, _html} = live(conn, ~p"/products/#{product.id}")
+
+      lv |> element("button", "Add to cart") |> render_click()
+
+      assert_redirected(lv, ~p"/users/log-in?next=/products/#{product.id}")
+    end
+
+    test "shows both buttons for unauthenticated users", %{conn: conn} do
+      product = product_fixture()
+      {:ok, _lv, html} = live(conn, ~p"/products/#{product.id}")
+
+      assert html =~ "Buy now"
+      assert html =~ "Add to cart"
     end
   end
 end
