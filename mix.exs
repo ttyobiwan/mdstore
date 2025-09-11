c981373e1060f28d3554842cb1dd4f43cd602074
ttyobiwan <piotr.tobiasz.dev@gmail.com>
2025-09-11 12:24:03 +0200

    bring back redix

--- mix.exs
+++ mix.exs
@@ -62,10 +62,10 @@ defmodule Mdstore.MixProject do
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:stripity_stripe, "~> 2.0"},
-      {:cachex, "~> 4.1"}
+      {:cachex, "~> 4.1"},
       # If you want to use redis instead of cachex
-      # {:redix, "~> 1.5"},
-      # {:castore, "~> 1.0"}
+      {:redix, "~> 1.5"},
+      {:castore, "~> 1.0"}
     ]
   end
 
